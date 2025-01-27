f378798b4c59de9b79e5a32e5c7fb22440e15e32
Geeten <geeten2301@gmail.com>
2025-01-27 22:58:06 +0530

    ui fixes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex h-[80vh] w-[90%] md:max-w-screen-5xl md:h-[960px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex h-[80vh] w-[90%] md:max-w-screen-5xl md:h-[860px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 			<Sidebar />
 			<MessageContainer />
 		</div>
